8d5131b29b17e6ae798ea6f4ebc1246fc11c09ba
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2023-07-11 10:39:24 -0600

    intitial

---
+++ twentyfive.js
@@ -0,0 +1,70 @@
+const second = 60;
+const minute = 60;
+const millisecond = 1000;
+let firstStart = true;
+let running = false;
+let justTicked = false;
+
+let totalTime = 0;
+let startTime = new Date();
+
+function format(number) {
+  return `${number < 10 ? "0" : ""}${number}`;
+}
+
+function formatTime(time) {
+  let seconds = Math.floor((time / millisecond) % second);
+  let hours = Math.floor(time / millisecond / second / minute);
+  let minutes = Math.floor((time / millisecond / second) % minute);
+  let timeString = `${format(hours)}:${format(minutes)}:${format(seconds)}`;
+
+  return timeString;
+}
+
+function calcTotalTime() {
+  let currentTime = new Date();
+  totalTime = currentTime - startTime;
+}
+
+function printTime() {
+  document.getElementById("total-time").innerHTML = formatTime(totalTime);
+  document.getElementById("start-time").innerHTML = startTime;
+}
+
+function tick() {
+  if (!justTicked) {
+    justTicked = true;
+    setTimeout(() => {
+      justTicked = false;
+      if (running) {
+        calcTotalTime();
+        printTime();
+        tick();
+      }
+    }, 1000);
+  }
+}
+
+function startClock() {
+  if (firstStart) {
+    firstStart = false;
+    startTime = new Date();
+  } else {
+    let currentTime = new Date();
+    startTime = new Date(currentTime - totalTime);
+  }
+  running = true;
+  tick();
+}
+
+function pause() {
+  running = false;
+}
+
+function reset() {
+  firstStart = true;
+  running = false;
+
+  totalTime = 0;
+  startTime = new Date();
+}
